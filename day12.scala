5a576e2d10adf279d6848ff35ef300e131721bd4
ogmobot <ogmobot@github.com>
2022-09-17 09:34:22 +1000

    Don't assume three-dimensional space for day 12

--- day12.scala
+++ day12.scala
@@ -4,89 +4,70 @@ import scala.util.matching.Regex
 import scala.math.BigInt
 
 class Moon(
-    var  x: Int,    var  y: Int,    var  z: Int,
-    var vx: Int,    var vy: Int,    var vz: Int
+    var pos: Array[Int]
 ) {
-    def applyGravity(other: Moon): Unit = {
-        if (other.x > x) vx += 1
-        if (other.x < x) vx -= 1
-        if (other.y > y) vy += 1
-        if (other.y < y) vy -= 1
-        if (other.z > z) vz += 1
-        if (other.z < z) vz -= 1
-    }
-    def update(): Unit = {
-        x += vx
-        y += vy
-        z += vz
-    }
-    def energy(): Int = (x.abs + y.abs + z.abs) * (vx.abs + vy.abs + vz.abs)
+    var vel: Array[Int] = Array.fill(pos.length)(0)
+    def applyGravity(other: Moon): Unit =
+        for (dim <- 0 until pos.length) {
+            if (other.pos(dim) > pos(dim)) vel(dim) += 1
+            if (other.pos(dim) < pos(dim)) vel(dim) -= 1
+        }
+    def update(): Unit =
+        for (dim <- 0 until pos.length)
+            pos(dim) += vel(dim)
+    def energy(): Int = pos.map{ _.abs }.sum * vel.map{ _.abs }.sum
 }
 
 // Logic functions
 
 def lowestCommonMultiple(a: BigInt, b: BigInt): BigInt = a * b / a.gcd(b)
 
-def applyGravityToAll(moons: List[Moon]): Unit = {
-    moons.map { m => {
-        moons.map { _.applyGravity(m) }
-    }}
-}
+def applyGravityToAll(moons: Array[Moon]): Unit =
+    moons.map(m => moons.map{ _.applyGravity(m) })
 
-def extractDimensions(moons: List[Moon]): List[List[Int]] = {
-    var xs = List.empty[Int]
-    var ys = List.empty[Int]
-    var zs = List.empty[Int]
-    for (moon <- moons) {
-        xs ++= List(moon.x, moon.vx)
-        ys ++= List(moon.y, moon.vy)
-        zs ++= List(moon.z, moon.vz)
-    }
-    return List(xs, ys, zs)
-}
+def extractDimensions(moons: Array[Moon]): Array[Array[Int]] =
+    moons.map(m => Array(m.pos, m.vel)).reduce((a, b) => a ++ b).transpose
 
 // IO functions
 
-def lineToMoon(line: String): Moon = {
+def lineToMoon3D(line: String): Moon = {
     val matcher: Regex = "<x=(-?[0-9]+), y=(-?[0-9]+), z=(-?[0-9]+)>".r
     line match {
-        case matcher(x, y, z) => new Moon(x.toInt, y.toInt, z.toInt, 0, 0, 0)
-        case _                => new Moon(0, 0, 0, 0, 0, 0)
+        case matcher(x, y, z) => new Moon(Array(x.toInt, y.toInt, z.toInt))
+        case _                => new Moon(Array.empty[Int])
     }
 }
 
-def printMoons(moons: List[Moon]): Unit = {
-    moons.map { m =>
-        println(s"<x=${m.x}, y=${m.y}, z=${m.z}> " ++
-            s"vel=<x=${m.vx}, y=${m.vy}, z=${m.vz}>")
-    }
-}
+def printMoons3D(moons: Array[Moon]): Unit =
+    moons.map(m =>
+        println(s"<x=${m.pos(0)}, y=${m.pos(1)}, z=${m.pos(2)}> " ++
+            s"vel=<x=${m.vel(0)}, y=${m.vel(1)}, z=${m.vel(2)}>")
+    )
 
 // Main function
 
 def main(): Unit = {
-    val moons: List[Moon] = Source.fromFile("input12.txt")
-                                  .getLines()
-                                  .map(lineToMoon)
-                                  .toList
-    val initial: List[List[Int]] = extractDimensions(moons)
-    var cycles: Array[Int] = Array(0, 0, 0)
+    val moons: Array[Moon] = Source.fromFile("input12.txt")
+                                   .getLines()
+                                   .map(lineToMoon3D)
+                                   .toArray
+    val initial: Array[Array[Int]] = extractDimensions(moons)
+    var cycles: Array[Int] = Array.fill(initial.length)(0)
     var timer: Int = 0
     while (cycles.min == 0) {
         applyGravityToAll(moons)
-        moons.map { _.update() }
+        moons.map{ _.update() }
         timer += 1
-        //printMoons(moons)
+        //printMoons3D(moons)
 
         // Part 1
         if (timer == 1000)
-            println(moons.map { _.energy() } .sum)
+            println(moons.map{ _.energy() }.sum)
         // Part 2
-        val current: List[List[Int]] = extractDimensions(moons)
-        for (dim <- (0 until cycles.length)) {
-            if (cycles(dim) == 0 && current(dim) == initial(dim))
+        val current: Array[Array[Int]] = extractDimensions(moons)
+        for (dim <- 0 until cycles.length)
+            if (cycles(dim) == 0 && current(dim).sameElements(initial(dim)))
                 cycles(dim) = timer
-        }
     }
     //println(s"${cycles(0)} ${cycles(1)} ${cycles(2)}")
     println(
