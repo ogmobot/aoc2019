4af93083ecc55d98e8bc45d772a154f672ac6c5b
ogmobot <ogmobot@github.com>
2022-09-17 10:01:07 +1000

    Fix potential bug in day 3 If a wire section ended on an existing wire section, the distances of the following wire section would be inaccurate.

--- day03.scala
+++ day03.scala
@@ -6,22 +6,25 @@ def manhattan(a: (Int, Int)): Int = a._1.abs + a._2.abs
 
 def putGrid(
     grid: mutable.Map[(Int, Int), Int],
-    start: (Int, Int),
+    start_dist: ((Int, Int), Int),
     wireSection: String
-): (Int, Int) = {
+): ((Int, Int), Int) = {
     val delta = wireSection(0) match {
         case 'U' => ( 0, -1)
         case 'D' => ( 0,  1)
         case 'L' => (-1,  0)
         case 'R' => ( 1,  0)
     }
-    val dist = grid.getOrElse(start, 0)
+    val (start, dist) = start_dist
     val amount = wireSection.substring(1).toInt
     (1 to amount).map { d => {
         val coord = (start._1 + (d * delta._1), (start._2 + (d * delta._2)))
         grid.update(coord, grid.getOrElse(coord, dist + d))
     }}
-    return (start._1 + (amount * delta._1), start._2 + (amount * delta._2))
+    return (
+        (start._1 + (amount * delta._1), start._2 + (amount * delta._2)),
+        dist + amount
+    )
 }
 
 def main() = {
@@ -30,13 +33,13 @@ def main() = {
     ).toList
     val grid_1 = mutable.Map.empty[(Int, Int), Int]
     val grid_2 = mutable.Map.empty[(Int, Int), Int]
-    var pt = (0, 0)
+    var pt_dist = ((0, 0), 0)
     for (w <- wires(0)) {
-        pt = putGrid(grid_1, pt, w)
+        pt_dist = putGrid(grid_1, pt_dist, w)
     }
-    pt = (0, 0)
+    pt_dist = ((0, 0), 0)
     for (w <- wires(1)) {
-        pt = putGrid(grid_2, pt, w)
+        pt_dist = putGrid(grid_2, pt_dist, w)
     }
     val intersections = grid_1.keys.toSet.intersect(grid_2.keys.toSet)
     // Part 1
