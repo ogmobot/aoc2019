1bb0989d8e3305461f178c043cf775865d0c48c5
ogmobot <ogmobot@github.com>
2022-09-17 10:05:28 +1000

    Remove some unnecessary brackets

--- day03.scala
+++ day03.scala
@@ -17,10 +17,10 @@ def putGrid(
     }
     val (start, dist) = start_dist
     val amount = wireSection.substring(1).toInt
-    (1 to amount).map { d => {
+    (1 to amount).map(d => {
         val coord = (start._1 + (d * delta._1), (start._2 + (d * delta._2)))
         grid.update(coord, grid.getOrElse(coord, dist + d))
-    }}
+    })
     return (
         (start._1 + (amount * delta._1), start._2 + (amount * delta._2)),
         dist + amount
@@ -28,24 +28,23 @@ def putGrid(
 }
 
 def main() = {
-    val wires = Source.fromFile("input03.txt").getLines().map(
-        _.split(",").toList
-    ).toList
+    val wires = Source.fromFile("input03.txt")
+                      .getLines()
+                      .map(_.split(",").toList)
+                      .toList
     val grid_1 = mutable.Map.empty[(Int, Int), Int]
     val grid_2 = mutable.Map.empty[(Int, Int), Int]
     var pt_dist = ((0, 0), 0)
-    for (w <- wires(0)) {
+    for (w <- wires(0))
         pt_dist = putGrid(grid_1, pt_dist, w)
-    }
     pt_dist = ((0, 0), 0)
-    for (w <- wires(1)) {
+    for (w <- wires(1))
         pt_dist = putGrid(grid_2, pt_dist, w)
-    }
     val intersections = grid_1.keys.toSet.intersect(grid_2.keys.toSet)
     // Part 1
     println(intersections.map(manhattan).min)
     // Part 2
-    println(intersections.map({ x => grid_1(x) + grid_2(x) }).min)
+    println(intersections.map(x => grid_1(x) + grid_2(x)).min)
 }
 
 main()
