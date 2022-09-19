14508c5ada2a1d6bf209aebbabb5b1edf4fb20b1
ogmobot <ogmobot@github.com>
2022-09-19 18:31:11 +1000

    Day 16: use struct instead of raw array in preparation for part 2

--- day16.c
+++ day16.c
@@ -1,24 +1,53 @@
+#include <malloc.h>
 #include <stdio.h>
 #include <stdint.h>
 
-#define MAX_DIGITS 1000
+/* Struct handling */
 
-void populate_buffer(char *filename, int32_t *buffer) {
+struct message {
+    size_t size;
+    int32_t *data;
+};
+
+struct message *new_message(size_t size) {
+    /* size should never be zero */
+    struct message *m = calloc(1, sizeof(struct message));
+    m->size = size;
+    m->data = calloc(size, sizeof(int32_t));
+    return m;
+}
+
+void free_message(struct message *m) {
+    free(m->data);
+    free(m);
+}
+
+/* IO handling */
+
+void populate_buffer(char *filename, struct message *m) {
     FILE *fp = fopen(filename, "r");
+    size_t index = 0;
     while (1) {
         char c = fgetc(fp);
         if ('0' <= c && c <= '9') {
-            *buffer++ = c - '0';
+            if (index >= m->size - 1) {
+                m->size *= 2;
+                m->data = realloc(m->data, sizeof(int32_t) * m->size);
+                /* doesn't initialise new memory */
+            }
+            (m->data)[index++] = c - '0';
         } else {
             break;
         }
     }
-    *buffer = -1;
+    m->size = index; /* Don't bother reallocating memory */
     fclose(fp);
     return;
 }
 
-int32_t pattern_at(size_t input_index, size_t output_pos, int32_t offset) {
+/* Program logic */
+
+int32_t pattern_at(size_t input_index, size_t output_pos) {
     if (output_pos > input_index) {
         return 0;
     } else if (output_pos * 2 > input_index) {
@@ -26,12 +55,12 @@ int32_t pattern_at(size_t input_index, size_t output_pos, int32_t offset) {
     } else {
         int32_t pattern[4] = {0, 1, 0, -1};
         if (output_pos == 0)
-            return pattern[(input_index + offset) % 4];
+            return pattern[(input_index + 1) % 4];
         size_t pattern_ptr = 0;
         size_t j = 1;
         for (int32_t i = 0; i < input_index; i++) {
             j++;
-            if (j == output_pos + offset) {
+            if (j == output_pos + 1) {
                 j = 0;
                 pattern_ptr = (pattern_ptr + 1) % 4;
             }
@@ -40,17 +69,17 @@ int32_t pattern_at(size_t input_index, size_t output_pos, int32_t offset) {
     }
 }
 
-void apply_transform(int32_t *buffer) {
-    for (int i = 0; buffer[i] != -1; i++) {
+void apply_transform(struct message *m) {
+    for (int i = 0; i < m->size; i++) {
         int32_t total = 0;
-        for (int j = 0; buffer[j] != -1; j++) {
+        for (int j = 0; j < m->size; j++) {
             /*
-            printf("%2d*%2d   ", buffer[j], pattern_at(j, i, 1));
+            printf("%2d*%2d   ", buffer[j], pattern_at(j, i));
             */
-            total += (buffer[j] * pattern_at(j, i, 1));
+            total += ((m->data)[j] * pattern_at(j, i));
         }
         if (total < 0) total = -total;
-        buffer[i] = total % 10;
+        (m->data)[i] = total % 10;
         /*
         printf(" = %d\n", total % 10);
         */
@@ -58,16 +87,18 @@ void apply_transform(int32_t *buffer) {
     return;
 }
 
+/* Main function */
+
 int main(void) {
-    int32_t buffer[MAX_DIGITS] = {0};
-    populate_buffer("input16.txt", buffer);
+    struct message *m = new_message(1024);
+    populate_buffer("input16.txt", m);
 
     for (int i = 0; i < 100; i++) {
-        apply_transform(buffer);
+        apply_transform(m);
     }
     printf("%d%d%d%d%d%d%d%d\n",
-        buffer[0], buffer[1], buffer[2], buffer[3],
-        buffer[4], buffer[5], buffer[6], buffer[7]);
+        m->data[0], m->data[1], m->data[2], m->data[3],
+        m->data[4], m->data[5], m->data[6], m->data[7]);
     return 0;
 }
 
