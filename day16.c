49e364e7855167d883136fcf4c80884694378939
ogmobot <ogmobot@github.com>
2022-09-20 19:02:13 +1000

    Day 16 in C (both parts)

--- day16.c
+++ day16.c
@@ -1,12 +1,14 @@
 #include <malloc.h>
 #include <stdio.h>
 #include <stdint.h>
+#include <string.h>
 
 /* Struct handling */
 
 struct message {
     size_t size;
     int32_t *data;
+    int32_t *next;
 };
 
 struct message *new_message(size_t size) {
@@ -14,12 +16,33 @@ struct message *new_message(size_t size) {
     struct message *m = calloc(1, sizeof(struct message));
     m->size = size;
     m->data = calloc(size, sizeof(int32_t));
+    m->next = calloc(size, sizeof(int32_t));
     return m;
 }
 
 void free_message(struct message *m) {
     free(m->data);
     free(m);
+}
+
+void swap_buffer(struct message *m) {
+    /* Swaps the values of the data and next pointers, so that it seems like
+       their contents have been swapped. (Will the compiler optimise it?) */
+    void *tmp = m->data;
+    m->data = m->next;
+    m->next = tmp;
+    return;
+}
+
+void multiply_length(struct message *m, size_t factor) {
+    size_t orig_size = m->size;
+    (m->size) *= factor;
+    m->data = realloc(m->data, sizeof(int32_t) * m->size);
+    m->next = realloc(m->next, sizeof(int32_t) * m->size);
+    for (size_t i = 1; i < factor; i++) {
+        memcpy(m->data + (i * orig_size), m->data, orig_size * sizeof(int32_t));
+    }
+    return;
 }
 
 /* IO handling */
@@ -31,7 +54,7 @@ void populate_buffer(char *filename, struct message *m) {
         char c = fgetc(fp);
         if ('0' <= c && c <= '9') {
             if (index >= m->size - 1) {
-                m->size *= 2;
+                m->size += 1024;
                 m->data = realloc(m->data, sizeof(int32_t) * m->size);
                 /* doesn't initialise new memory */
             }
@@ -40,7 +63,10 @@ void populate_buffer(char *filename, struct message *m) {
             break;
         }
     }
-    m->size = index; /* Don't bother reallocating memory */
+    m->size = index;
+    m->data = realloc(m->data, sizeof(int32_t) * m->size);
+    m->next = realloc(m->next, sizeof(int32_t) * m->size);
+    /* doesn't initialise new memory */
     fclose(fp);
     return;
 }
@@ -58,7 +84,7 @@ int32_t pattern_at(size_t input_index, size_t output_pos) {
             return pattern[(input_index + 1) % 4];
         size_t pattern_ptr = 0;
         size_t j = 1;
-        for (int32_t i = 0; i < input_index; i++) {
+        for (size_t i = 0; i < input_index; i++) {
             j++;
             if (j == output_pos + 1) {
                 j = 0;
@@ -70,21 +96,85 @@ int32_t pattern_at(size_t input_index, size_t output_pos) {
 }
 
 void apply_transform(struct message *m) {
-    for (int i = 0; i < m->size; i++) {
-        int32_t total = 0;
-        for (int j = 0; j < m->size; j++) {
-            /*
-            printf("%2d*%2d   ", buffer[j], pattern_at(j, i));
-            */
-            total += ((m->data)[j] * pattern_at(j, i));
-        }
-        if (total < 0) total = -total;
-        (m->data)[i] = total % 10;
-        /*
-        printf(" = %d\n", total % 10);
-        */
+    /* Part 2:
+       My first instinct was to construct a size * size matrix A, then find
+       eigenvectors and eigenvalues of A, use them to compute A^100, then
+       find (A^100)m.
+       Fortunately, we can exploit the structure of A to do this without
+       needing to allocate terabytes of memory.
+    */
+    const int32_t deltas[4] = {1, -1, -1, 1};
+    size_t delta_index = 0;
+    size_t step_size = 1;
+    for (size_t i = 0; i < m->size; i++) {
+        (m->next)[i] = 0;
     }
+    while (step_size <= m->size) {
+        /* Step down to the bottommost index of the "matrix"... */
+        int32_t read_head = step_size - 1;
+        int32_t write_head = (m->size / step_size) - 1;
+        while (read_head < m->size)
+            read_head += step_size;
+        /*
+        printf("step size %lu\n", step_size);
+        */
+        /* Then step back up, adding as we go */
+        size_t timer = 0;
+        int32_t subtotal = 0;
+        while (read_head >= 0) {
+            read_head--;
+            timer++;
+            if (read_head >= 0 && read_head < m->size) {
+                subtotal += (m->data)[read_head];
+            }
+            if (write_head >= 0
+                && write_head < m->size
+                && timer % step_size == 0) {
+                (m->next)[write_head--] += deltas[delta_index] * subtotal;
+                /*
+                printf("subtotal = %d\n", subtotal);
+                printf("%d %d %d %d %d %d %d %d\n",
+                    m->next[0], m->next[1], m->next[2], m->next[3],
+                    m->next[4], m->next[5], m->next[6], m->next[7]);
+                */
+            }
+        }
+        delta_index = (delta_index + 1) % 4;
+        step_size++;
+    }
+    for (size_t i = 0; i < m->size; i++) {
+        (m->next)[i] %= 10;
+        if ((m->next)[i] < 0)
+            m->next[i] *= -1;
+    }
+    swap_buffer(m);
     return;
+    /* Hm... This approach is ~6 times faster, but still too slow for part 2 */
+}
+
+void apply_first_pass(struct message *m, size_t offset) {
+    /* Quick 'n' dirty version of part 2; solve a simplified problem by relying
+       on the fact that we don't need any of the numbers in the first half of
+       the result. (Skips more than half of the computation!)
+       We can even skip filling in *everything* lower than the offset.
+    */
+    int32_t index = m->size - 1;
+    int32_t subtotal = 0;
+    while (index >= offset) {
+        subtotal += (m->data)[index];
+        (m->next)[index--] = subtotal % 10; /* Can't even be negative */
+    }
+    swap_buffer(m);
+    return;
+}
+
+size_t get_offset(struct message *m) {
+    size_t offset = 0;
+    for (size_t i = 0; i < 7; i++) {
+        offset *= 10;
+        offset += (m->data)[i];
+    }
+    return offset;
 }
 
 /* Main function */
@@ -93,13 +183,25 @@ int main(void) {
     struct message *m = new_message(1024);
     populate_buffer("input16.txt", m);
 
-    for (int i = 0; i < 100; i++) {
+    for (size_t i = 0; i < 100; i++) {
         apply_transform(m);
     }
     printf("%d%d%d%d%d%d%d%d\n",
         m->data[0], m->data[1], m->data[2], m->data[3],
         m->data[4], m->data[5], m->data[6], m->data[7]);
+
+    populate_buffer("input16.txt", m);
+    size_t offset = get_offset(m);
+    multiply_length(m, 10000);
+
+    for (size_t i = 0; i < 100; i++) {
+        apply_first_pass(m, offset); /* Assume offset > (size / 2) */
+    }
+    printf("%d%d%d%d%d%d%d%d\n",
+        m->data[offset + 0], m->data[offset + 1], m->data[offset + 2],
+        m->data[offset + 3], m->data[offset + 4], m->data[offset + 5],
+        m->data[offset + 6], m->data[offset + 7]);
+
+    free_message(m);
     return 0;
 }
-
-/* We'll need to go bigger for part 2... */
