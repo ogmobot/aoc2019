8210b2b54e6d5c0674f591258a76fa90bc02fc2f
ogmobot <ogmobot@github.com>
2022-09-21 14:46:46 +1000

    Day 20 (part 2) in Python

--- day20.py
+++ day20.py
@@ -72,7 +72,39 @@ class PortalMaze:
                     to_search.append([adj] + path)
         return None
 
+class RecursiveMaze(PortalMaze):
+    def __init__(self, template: PortalMaze):
+        self.grid = template.grid
+        self.start = (template.start[0], template.start[1], 0)
+        self.end = (template.end[0], template.end[1], 0)
+        self.inner_portals = {}
+        self.outer_portals = {}
+
+        portals = template.portals
+        min_row = min(k[0] for k in portals)
+        max_row = max(k[0] for k in portals)
+        min_col = min(k[1] for k in portals)
+        max_col = max(k[1] for k in portals)
+        for p, q in portals.items():
+            if p[0] in [min_row, max_row] or p[1] in [min_col, max_col]:
+                self.outer_portals[p] = q
+            else:
+                self.inner_portals[p] = q
+
+    def get_adjacent(self, coord):
+        res = []
+        for delta in ((1, 0), (-1, 0), (0, 1), (0, -1)):
+            target = (coord[0] + delta[0], coord[1] + delta[1], coord[2])
+            if self.grid[target[:2]] == '.' or target in [self.start, self.end]:
+                res.append(target)
+        if coord[:2] in self.inner_portals:
+            res.append(self.inner_portals[coord[:2]] + (coord[2] + 1,))
+        if coord[:2] in self.outer_portals and coord[2] > 0:
+            res.append(self.outer_portals[coord[:2]] + (coord[2] - 1,))
+        return res
+
 def main():
+    # Part 1
     maze = PortalMaze("input20.txt")
     path = maze.find_path(maze.start, maze.end)
     #print(path)
@@ -82,6 +114,11 @@ def main():
         #if p in maze.portals:
             #portals.append(maze.portal_names[p])
     #print(portals)
+
+    # Part 2 (Takes ~10 min)
+    rec_maze = RecursiveMaze(maze)
+    path = rec_maze.find_path(rec_maze.start, rec_maze.end)
+    print(len(path) - 1)
 
 if __name__ == "__main__":
     main()
