01b8d5d6456a0b2cbe6b0d2757935b7837c7d309
ogmobot <ogmobot@github.com>
2022-10-21 22:58:43 +1100

    Intcode: day 21 in C

---
+++ intcode/day21.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "intcode.h"
+
+int main(void) {
+    num_t file_buffer[8192];
+    size_t n;
+
+    n = load_file("input21.txt", file_buffer);
+    struct intcode_vm *vm = vm_from_buffer(file_buffer, n);
+    run_vm_interactive(vm, I_ASCII);
+    free_vm(vm);
+    return 0;
+}
